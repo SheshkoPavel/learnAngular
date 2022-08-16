a32d6be87dc2dad96944cb3ce4bcaf5e3d0d1fd9
Sheshko Pavel <derzurb@gmail.com>
2022-08-16 14:48:35 +0300

    feat: server request for products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
 import {products as data} from './data/products'
+import { ProductService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'first angular app';
-  products: IProduct[] = data;
+export class AppComponent implements OnInit {
+  
+  title = 'first angular app'
+  products: IProduct[] = []
+
+  constructor(private productsServices: ProductService){}
+
+  ngOnInit(): void {
+    this.productsServices.getAll().subscribe((products)=> {
+      this.products = products
+      
+    })
+  }
 }
